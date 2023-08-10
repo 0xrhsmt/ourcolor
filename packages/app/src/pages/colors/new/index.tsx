25681c39f9fa7ead835e60f1c041100091050810
ryo hashimoto <ryo.hashimoto.2525@gmail.com>
2023-08-10 18:01:47 +0900

    install react-hook-form

--- packages/app/src/pages/colors/new/index.tsx
+++ packages/app/src/pages/colors/new/index.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
-import { ZORA_REWARDS_DOCS_URL } from '../../../constants';
+import { useForm, useFieldArray } from 'react-hook-form';
 import { PlusSmallIcon } from '@heroicons/react/24/solid';
 
+import { ZORA_REWARDS_DOCS_URL } from '../../../constants';
+
+type FormValues = {
+  colors: {
+    tokenId: number;
+    quantity: number;
+  }[];
+};
+
 const NewPage: React.FC = () => {
+  const { register, control, handleSubmit } = useForm<FormValues>({
+    defaultValues: {
+      colors: [
+        {
+          tokenId: 0,
+          quantity: 0,
+        },
+      ],
+    },
+  });
+  const { fields, append, remove } = useFieldArray({
+    name: 'colors',
+    control,
+  });
+  const onSubmit = (data: FormValues) => console.log(data);
+
   return (
     <div>
       <div className="grid grid-cols-2 h-screen">
@@ -16,7 +41,7 @@ const NewPage: React.FC = () => {
               <h3 className="text-3xl font-bold mb-8">New Color</h3>
 
               <div>
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                   <div className="font-light mb-6">
                     Mix your colors and create a new color.
                     <br />
@@ -36,37 +61,77 @@ const NewPage: React.FC = () => {
                     .
                   </div>
 
-                  <div className="flex flex-col mb-6 space-y-2">
-                    <div className="flex flex-row space-x-4">
-                      <div className="w-full">
-                        <label className="label">
-                          <span className="label-text">base colors</span>
-                        </label>
-                        <select className="select select-bordered select-lg w-full">
-                          <option disabled selected>
-                            rgb (1,2,3)
-                          </option>
-                          <option>Large Apple</option>
-                          <option>Large Orange</option>
-                          <option>Large Tomato</option>
-                        </select>
-                      </div>
+                  <div className="flex flex-col mb-2 space-y-2">
+                    {fields.map((field, index) => {
+                      return (
+                        <div key={field.id}>
+                          <div className="flex flex-row">
+                            <div className="w-full">
+                              <label className="label">
+                                <span className="label-text">base colors</span>
+                              </label>
 
-                      <div className="w-full max-w-[100px]">
-                        <label className="label">
-                          <span className="label-text">quantity</span>
-                        </label>
-                        <input
-                          type="text"
-                          maxLength={3}
-                          className="input input-bordered input-lg w-full text-center max-w-[100px]"
-                        />
-                      </div>
-                    </div>
+                              <select
+                                className="select select-bordered select-lg w-full"
+                                {...register(
+                                  `colors.${index}.tokenId` as const,
+                                  {
+                                    required: true,
+                                  }
+                                )}
+                              >
+                                <option value={0} selected>
+                                  rgb (0,0,0)
+                                </option>
+                                <option value={1}>rgb (1,0,0)</option>
+                                <option value={2}>rgb (0,1,0)</option>
+                                <option value={3}>rgb (0,0,1)</option>
+                              </select>
+                            </div>
+
+                            <div className="w-full max-w-[100px] ml-4">
+                              <label className="label">
+                                <span className="label-text">quantity</span>
+                              </label>
+
+                              <input
+                                type="text"
+                                maxLength={3}
+                                className="input input-bordered input-lg w-full text-center max-w-[100px]"
+                                {...register(
+                                  `colors.${index}.quantity` as const,
+                                  {
+                                    valueAsNumber: true,
+                                    required: true,
+                                  }
+                                )}
+                              />
+                            </div>
+                          </div>
+                          <div className="flex justify-end mt-1.5">
+                            <button
+                              type="button"
+                              className="btn btn-link btn-sm pr-0"
+                              onClick={() => remove(index)}
+                            >
+                              Delete
+                            </button>
+                          </div>
+                        </div>
+                      );
+                    })}
                   </div>
 
                   <div className="flex flex-row justify-center items-center">
-                    <button className="btn btn-ghost">
+                    <button
+                      className="btn btn-ghost"
+                      onClick={() =>
+                        append({
+                          tokenId: 0,
+                          quantity: 0,
+                        })
+                      }
+                    >
                       <PlusSmallIcon height={24} width={24} />
                       Add Color
                     </button>
